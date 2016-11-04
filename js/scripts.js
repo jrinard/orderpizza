7eb2ee1f50919f96a4519ba6cb38663ed1160c34
Joshua Rinard <jrinard@hotmail.com>
2016-11-04 12:01:47 -0700

    Experimentation on grabbing multiple check boxes

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 function Pizza(name, cheeseX, toppingx, pineapple, size) {
   this.name = name;
   this.cheese = cheeseX;
-  this.topping = toppingx; // placeholder to be assigned]
+  this.topping = []; // placeholder to be assigned]
   this.fruit = pineapple;
   this.pizzaSize = size;
 }
@@ -12,8 +12,11 @@ function Pizza(name, cheeseX, toppingx, pineapple, size) {
 var pizza = new Pizza();
 
 //Prototype to calculate price depending on size
-Pizza.prototype.pizzaCost = function() {
+Pizza.prototype.pizzaCost = function(topping) {
   var finalPrice = 0;
+  var finalTop = 0;
+
+
   if (this.pizzaSize === "small"){
     finalPrice += 8;
   } else if (this.pizzaSize === "medium"){
@@ -26,16 +29,38 @@ Pizza.prototype.pizzaCost = function() {
     } else if (this.cheese === "extra"){
       finalPrice = finalPrice + 1;
     }
-      if (this.topping === "pepperoni"){
-        finalPrice = finalPrice + .5;
-      } else if (this.topping === "sausage"){
-        finalPrice = finalPrice + .5;
-      }
 
+  // var x = pizza.topping.length; console.log("top " + x);
+  //   finalPrice +=  x;
+
+
+    // var finalPrice = pizza.topping.length + 1; console.log("Toppings " + finalPrice);
+    // console.log("extra toppings " + pizza.topping.length);
+
+        for (var i = 1; i <= pizza.topping.length; i += 1) {
+              var finalTopping = pizza.topping[i] + 50;console.log("Final topping");
+            }
+
+    // for (var i = 0; i < pizza.topping.length; i += 1)
+    //   if (this.topping === "Pepperoni"){ console.log("pep1");
+    //     finalPrice = finalPrice + .5;
+    //   } else if (this.topping[1] === "sas"){console.log("sas1");
+    //     finalPrice = finalPrice + .5;
+    //   }
+
+    // var count = 0;
+    // for (var i = -1; i < pizza.topping.length; i += 1) {
+    //     count += 1;
+    //     console.log("Toppings " + count);
+    //     if (count > 1){
+    //       finalPrice = count + finalPrice;
+    //     }
+    //   }
 
   console.log("pizzaCost has been set to " + finalPrice);
   return finalPrice;
 };
+
 
 // !!! idea for cheese if they pick a topping then push a value to an array.
 // Calculate the length and multiply to affect price.
@@ -64,6 +89,15 @@ $(document).ready(function(){
 
   var newPizza = new Pizza(inputtedName,cheeseAmount, toppingChoice, veggieType, sizeSelection)
 
+
+
+  // pizza.topping.forEach(function() {
+  //   var x = 0;
+  //   x +=1;
+  //   console.log("result " + x);
+  //
+  // });
+
   var finalPrice = newPizza.pizzaCost();
 
   // call toppings?
@@ -71,8 +105,9 @@ $(document).ready(function(){
   $("#results").append("Order Received");
   $("#results").append("<br>" +  "Toppings Include: ");
   $("input:checkbox[name=topping]:checked").each(function(){
-    var finalToppingChoice = $(this).val();
-    $('#results').append(finalToppingChoice + " ");
+    pizza.topping.push($(this).val());
+    // var finalToppingChoice = $(this).val();
+    // $('#results').append(finalToppingChoice + " ");
   });
 $("#results").append("<br>" + newPizza.name + " " + "your pizza will cost $" + finalPrice);
 
